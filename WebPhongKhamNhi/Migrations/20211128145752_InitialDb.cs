c8b28cb851cadccde9c0b626ebfc282620920e62
minhduyet129 <minhduyet129@gmail.com>
2021-11-28 22:03:49 +0700

    Update DBfull

---
+++ WebPhongKhamNhi/Migrations/20211128145752_InitialDb.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace WebPhongKhamNhi.Migrations
+{
+    public partial class InitialDb : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.AddColumn<DateTime>(
+                name: "NgayTao",
+                table: "HOSOKHAM",
+                type: "datetime2",
+                nullable: false,
+                defaultValue: new DateTime(1, 1, 1, 0, 0, 0, 0, DateTimeKind.Unspecified));
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropColumn(
+                name: "NgayTao",
+                table: "HOSOKHAM");
+        }
+    }
+}
