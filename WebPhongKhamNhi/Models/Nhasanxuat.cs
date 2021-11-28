88f7fdfd6721569d20599e6a8490984ab44ed5cf
DoHoangTrung <63000950+DoHoangTrung@users.noreply.github.com>
2021-11-28 23:25:12 +0700

    CRUD nhà sản xuất

--- WebPhongKhamNhi/Models/Nhasanxuat.cs
+++ WebPhongKhamNhi/Models/Nhasanxuat.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
 
 #nullable disable
 
@@ -13,9 +14,17 @@ namespace WebPhongKhamNhi.Models
             Thuocs = new HashSet<Thuoc>();
         }
 
+
         public int MaNhaSanXuat { get; set; }
+
+        [Display(Name ="Tên nhà sản xuất")]
+        [Required(ErrorMessage ="{0} không được bỏ trống.")]
         public string TenNhaSanXuat { get; set; }
         public string DiaChi { get; set; }
+
+        [Display(Name ="Số điện thoại")]
+        [Required(ErrorMessage = "{0} không được bỏ trống.")]
+        [StringLength(10, MinimumLength =9, ErrorMessage ="{0} có từ {2} đến {1} chữ số")]
         public string SoDienThoai { get; set; }
 
         public virtual ICollection<Phieunhap> Phieunhaps { get; set; }
