1e724a8d7ec5c83b1244a6b88b21bd1e2429b76b
DoHoangTrung <63000950+DoHoangTrung@users.noreply.github.com>
2021-12-04 22:47:29 +0700

    CRUD dich vu kham CRUD phieu dang ky kham xem lịch sử đăng ký khám trong thông tin bệnh nhân

--- WebPhongKhamNhi/Controllers/PhieuNhapThuocController.cs
+++ WebPhongKhamNhi/Controllers/PhieuNhapThuocController.cs
@@ -27,6 +27,31 @@ namespace WebPhongKhamNhi.Controllers
             return View(listPhieuNhap);
         }
 
+        //POST:PhieuNhapThuoc/Index
+        [HttpPost]
+        public IActionResult Index(string textSearch)
+        {
+            if (textSearch == null)
+            {
+                return RedirectToAction("Index");
+            }
+
+            string textSearchFormat = textSearch.Trim().ToLower();
+            if (string.IsNullOrEmpty(textSearchFormat))
+            {
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                var result = _context.Phieunhaps.Include(p => p.MaNhaSanXuatNavigation).Where(
+                   p => p.MaNhaSanXuatNavigation.TenNhaSanXuat.Contains(textSearchFormat) ||
+                   p.MaPhieuNhap.ToString().Contains(textSearchFormat)
+               ).ToList();
+
+                return View(result);
+            }
+        }
+
         // GET: PhieuNhapThuoc/Details/5
         public ActionResult Details(int id)
         {
@@ -47,7 +72,7 @@ namespace WebPhongKhamNhi.Controllers
         // GET: PhieuNhapThuoc/Create
         public ActionResult Create()
         {
-            
+
             var listNsx = _context.Nhasanxuats.OrderBy(n => n.TenNhaSanXuat);
             var listThuoc = _context.Thuocs.OrderBy(t => t.Ten);
 
@@ -61,57 +86,107 @@ namespace WebPhongKhamNhi.Controllers
             return View();
         }
 
+
+
         // POST: PhieuNhapThuoc/Create
         [HttpPost]
         public ActionResult Create(Phieunhap phieunhap, IEnumerable<Chitietphieunhap> chiTietPhieus)
         {
-
             try
             {
+                //vì id tự sinh, lưu vào csdl để lấy id
+                if (!ModelState.IsValid)
+                {
+                    return Json(new { status = false, message = "dữ liệu không hợp lệ" });
+
+                }
                 _context.Phieunhaps.Add(phieunhap);
                 _context.SaveChanges();
                 Phieunhap phieunhapInDb = _context.Phieunhaps.Where(p => p.NgayNhap == phieunhap.NgayNhap).FirstOrDefault();
-                if(phieunhapInDb != null)
+                if (phieunhapInDb == null && chiTietPhieus == null)
                 {
-                    foreach (var ct in chiTietPhieus)
-                    {
-                        ct.MaPhieuNhap = phieunhap.MaPhieuNhap;
-                        _context.Chitietphieunhaps.Add(ct);
-                    }
-                    _context.SaveChanges();
-                    return Json(new { status = true, redirectToUrl = Url.Action("Index", "PhieuNhapThuoc") });
+                    return Json(new { status = false, message = "Tham số truyền vào null" });
                 }
-                else
+
+
+                foreach (var ct in chiTietPhieus)
                 {
-                    return Json(new { status = false});
+                    ct.MaPhieuNhap = phieunhap.MaPhieuNhap;
+                    _context.Chitietphieunhaps.Add(ct);
+
+                    //nếu mã thuốc của chi tiết = null, không lưu nữa, xoá phiếu nhập vừa thêm
                 }
+
+                _context.SaveChanges();
+
+                return Json(new { status = true, redirectToUrl = Url.Action("Index", "PhieuNhapThuoc") });
+
             }
             catch (Exception ex)
             {
-                return Json(new { status= false , message = ex.Message});
+                return Json(new { status = false, message = ex.Message });
             }
-
         }
 
         // GET: PhieuNhapThuoc/Edit/5
         public ActionResult Edit(int id)
         {
+            var listNsx = _context.Nhasanxuats.OrderBy(n => n.TenNhaSanXuat);
+            var listThuoc = _context.Thuocs.OrderBy(t => t.Ten);
+            var phieuNhap = _context.Phieunhaps.Find(id);
+            var chiTiets = _context.Chitietphieunhaps.Where(ct => ct.MaPhieuNhap == id).ToList();
+
+
+            if (listNsx == null || listThuoc == null)
+            {
+                return NotFound();
+            }
+            ViewBag.listNsx = listNsx;
+            ViewBag.listThuoc = listThuoc;
+            ViewBag.chiTiets = chiTiets;
+            ViewBag.phieuNhap = phieuNhap;
+
             return View();
         }
 
         // POST: PhieuNhapThuoc/Edit/5
         [HttpPost]
         [ValidateAntiForgeryToken]
-        public ActionResult Edit(int id, IFormCollection collection)
+        public ActionResult Edit(int maPhieuNhap, Phieunhap phieuNhap, IEnumerable<Chitietphieunhap> chiTietPhieus)
         {
-            try
+            Phieunhap phieuUpdate = _context.Phieunhaps.Find(maPhieuNhap);
+            if (phieuNhap == null)
             {
-                return RedirectToAction(nameof(Index));
+                return NotFound();
             }
-            catch
+
+            phieuUpdate.MaNhaSanXuat = phieuNhap.MaNhaSanXuat;
+            phieuUpdate.NgayNhap = phieuNhap.NgayNhap;
+            phieuUpdate.TongTien = phieuNhap.TongTien;
+
+            _context.Update(phieuUpdate);
+
+            //cập nhật phiếu nhập: 2 hướng:
+            //1. xoá -> thêm lại
+            //2.
+
+            //xoa 
+            var chiTietsHienTai = _context.Chitietphieunhaps.Where(c => c.MaPhieuNhap == maPhieuNhap);
+            foreach (var ct in chiTietsHienTai)
             {
-                return View();
+                _context.Chitietphieunhaps.Remove(ct);
             }
+
+            //them lai
+            foreach (var ct in chiTietPhieus)
+            {
+                ct.MaPhieuNhap = maPhieuNhap;
+                _context.Chitietphieunhaps.Add(ct);
+            }
+
+            _context.SaveChanges();
+
+            return Json(new { status = true, redirectToUrl = Url.Action("Index", "PhieuNhapThuoc") });
         }
 
         // GET: PhieuNhapThuoc/Delete/5
@@ -127,19 +202,18 @@ namespace WebPhongKhamNhi.Controllers
         public ActionResult DeletePost(int MaPhieuNhap)
         {
             Phieunhap phieuXoa = _context.Phieunhaps.Find(MaPhieuNhap);
-            if(phieuXoa == null)
+            if (phieuXoa == null)
             {
                 return NotFound();
             }
             //xoá chi tiết phiếu nhập trước
             var chiTiets = _context.Chitietphieunhaps.Where(c => c.MaPhieuNhap == MaPhieuNhap);
-            if(chiTiets.Count() == 0)
+            if (chiTiets.Count() != 0)
             {
-                return NotFound();
-            }
-            foreach (var ct in chiTiets)
-            {
-                _context.Chitietphieunhaps.Remove(ct);
+                foreach (var ct in chiTiets)
+                {
+                    _context.Chitietphieunhaps.Remove(ct);
+                }
             }
 
             //xoá nốt phiếu nhập
